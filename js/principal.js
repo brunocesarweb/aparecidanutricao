d565e27af4deb6870e8c2204be94fb83e7738248
Bruno C. A. Gaudino <bruno.cesar@viewit.com.br>
2017-03-16 17:36:15 -0300

    Bruno Cesar - primeiras aulas estrutura do projeto

---
+++ js/principal.js
@@ -0,0 +1,7 @@
+var titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.textContent);
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+console.log(paciente);
