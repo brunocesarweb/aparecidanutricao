e57008782dcb3741a4a2a9269e444a7bcc332d9d
Bruno C. A. Gaudino <bruno.cesar@viewit.com.br>
2017-03-31 16:22:59 -0300

    Bruno Cesar - Quebrando o código, criando funções

--- js/form.js
+++ js/form.js
@@ -1,43 +1,3 @@
-var titulo = document.querySelector("h1");
-
-var pacientes = document.querySelectorAll(".paciente");
-
-for(var i = 0; i < pacientes.length; i++){
-
-  var paciente = pacientes[i];
-
-  var tdPeso = paciente.querySelector(".info-peso");
-  var peso = tdPeso.textContent;
-
-  var tdAltura = paciente.querySelector(".info-altura");
-  var altura = tdAltura.textContent;
-
-  var tdImc = paciente.querySelector(".info-imc");
-
-  var pesoEhValido = true;
-  var alturaEhValida = true;
-
-  if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!";
-    paciente.classList.add("paciente-invalido");
-  }
-
-  if (altura <= 0 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!";
-    paciente.classList.add("paciente-invalido");
-  }
-
-  if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc.toFixed(2);
-  }
-
-}//end - for
-
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
@@ -61,11 +21,13 @@ botaoAdicionar.addEventListener("click", function(event) {
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
+    imcTd.textContent = calculaImc(peso,altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
 
     var tabela = document.querySelector("#tabela-pacientes");
 
